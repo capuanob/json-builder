bf914fe2774f99e3ef0d1e271ed216b001547308
bcapuano <bcapuano@asu.edu>
2022-05-31 11:37:37 -0700

    Fuzzer fixes

--- fuzz/fuzz.c
+++ fuzz/fuzz.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
+#include <stdlib.h>
+
 #include "json-builder.h"
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     // Create a JSON array out of fuzzer data
     json_value *arr = json_array_new(size);
-    json_array_push(arr, json_string_new_length((unsigned int) size, data);
+    json_array_push(arr, json_string_new_length((unsigned int) size, (const char*) data));
 
     // Create a buffer for serialization and serialize
     char *buf = (char*) malloc(json_measure(arr));
